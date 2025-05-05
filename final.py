0e5aecfcab1118fc8adde7f1b473ec6584f9a701
james <james.paton3@gmail.com>
2025-05-06 09:01:04 +1200

    activity fee calculater

--- final.py
+++ final.py
@@ -1,6 +1,18 @@
 name = input("what is your name: ")
 age = input("what is your age: ")
 
+
+
+
+
+
+
+
+
+
+
+
+
 activity_choice = ["1. Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ", "2. Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ", "3. Mountain biking. This is for 4 days and is considered “difficult” and costs $900. '"]
 
 print('choose an activity')
@@ -18,3 +30,9 @@ while True:
      except ValueError:
          print("Invalid input. Please enter a valid number.")
 
+if activity_choice == 1:
+    activity_fee = 800
+elif activity_choice == 2:
+    activity_fee = 400
+elif activity_choice == 3:
+    activity_fee = 900
