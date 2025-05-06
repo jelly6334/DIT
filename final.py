1af26e6249d739cf07a624ecd61371b7aef0532b
james <james.paton3@gmail.com>
2025-05-07 10:13:26 +1200

    started transport comfermation and calculater

--- final.py
+++ final.py
@@ -36,3 +36,27 @@ elif activity_choice == 2:
     activity_fee = 400
 elif activity_choice == 3:
     activity_fee = 900
+
+
+
+
+transport = ["1.yes", "2.no"]
+
+print('campers can make there own way to the camp or take a shuttle for $80. will you be taking a shuttel or making your way there?')
+print(f'{transport[0]}')
+print(f'{transport[1]}')
+
+while True:
+     try:
+         activity_choice = int(input("Enter the number of your chosen activity: "))
+         if activity_choice in [1, 2,]:
+             break
+         else:
+             print("Invalid input. Please enter a number between 1 and 3.")
+     except ValueError:
+         print("Invalid input. Please enter a valid number.")
+
+if transport == 1:
+    transport = 80
+elif transport == 2:
+    transport = 0
