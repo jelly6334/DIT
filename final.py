109778ef9380b88217458c6459377f72bcd4389c
james <james.paton3@gmail.com>
2025-05-14 10:23:42 +1200

    added coments

--- final.py
+++ final.py
@@ -1,7 +1,11 @@
-name = input("enter your name pleas: ")
- 
+
+# name input
+name = input("enter your name pleas: ") 
+
+# blanck line
 print()
 
+# age input
 while True:
      try:
          age = int(input(f"Hi {name}, please enter your age: "))
@@ -12,16 +16,17 @@ while True:
      except ValueError:
          print("Invalid input. Please enter a valid number for your age.")
 
-
+# Age validation
 if age < 5:
      print("Sorry, you must be 5 or older to participate.")
 if age > 17:
      print("Sorry, you must be 17 or younger to participate.")
 
+# blanck line
 print()
 
 #-------------------------------------------------------------------------------------------------------------
-
+# activity list
 activity_choice = ["1. Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ", "2. Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ", "3. Mountain biking. This is for 4 days and is considered “difficult” and costs $900. '"]
 
 print('choose an activity')
@@ -29,6 +34,7 @@ print(f'{activity_choice[0]}')
 print(f'{activity_choice[1]}')
 print(f'{activity_choice[2]}')
 
+# activity validation
 while True:
      try:
          activity_choice = int(input("Enter the number of your chosen activity: "))
@@ -39,6 +45,7 @@ while True:
      except ValueError:
          print("Invalid input. Please enter a valid number.")
 
+# activity calculator
 if activity_choice == 1:
     activity_fee = 800
 elif activity_choice == 2:
@@ -46,10 +53,12 @@ elif activity_choice == 2:
 elif activity_choice == 3:
     activity_fee = 900
 
+# blanck line
 print()
 
 #-------------------------------------------------------------------------------------------------------------
 
+# shuttle option
 def def_shuttle():
     while True:
         Shuttle = input('Do you require a shuttle bus for $80? (yes/no) (y/n): ')
@@ -65,6 +74,7 @@ def def_shuttle():
 
 #-------------------------------------------------------------------------------------------------------------
 
+# meal list
 meal_options = ["1.standerd meal", "2.vegetarian", "3.vegan"]
 
 print('select a meal')
@@ -72,6 +82,7 @@ print(f'{meal_options[0]}')
 print(f'{meal_options[1]}')
 print(f'{meal_options[2]}')
 
+# meal validation
 while True:
      try:
          meal_options = int(input("Enter the number of your chosen activity: "))
@@ -84,14 +95,16 @@ while True:
 
 #-------------------------------------------------------------------------------------------------------------
 
-
-
+# shuttle print out
 shuttle_fee = def_shuttle()
 print(f'Your shuttle bus fee is: ${shuttle_fee}')
 
 total_fee = activity_fee + shuttle_fee
 
+# blanck line
+print()       
 
-
-print()         
+#-------------------------------------------------------------------------------------------------------------
+ 
+# final print out
 print(f'{name}, aged {age}, has chosen activity option "{activity_choice}", meal option: "{meal_options}". The total fee is ${total_fee}.')
